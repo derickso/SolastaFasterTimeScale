01caa5b53b32cccf9762a3478445102f3ad71a63
derickso <derickso@uci.edu>
2021-08-17 18:45:08 -0700

    Initial upload

---
+++ SolastaFasterTimeScale/SolastaFasterTimescale/Patches/GameTimeSetTimeScalePatcher.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace SolastaFasterTimeScale.Patches
+{
+    internal static class GameTimeSetTimeScalePatcher
+    {
+        [HarmonyPatch(typeof(GameTime), "SetTimeScale")]
+        static class GameTime_SetTimeScale_Patch
+        {
+            static bool Prefix(ref float ___timeScale, ref bool ___fasterTimeMode)
+            {
+                Time.timeScale = ___timeScale * (___fasterTimeMode ? Viewers.ModSettingsViewer.CustomTimeScale : 1f);
+
+                return false;
+            }
+        }
+    }
+}
